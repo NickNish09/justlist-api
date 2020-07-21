50bb7c0a9bce613ef537411e5a4e0128a8b1b4b1
nicknish09 <nnmarques97@gmail.com>
2020-07-20 22:50:38 -0300

    mongoose disconnect after test

--- tests/integration/pages.test.ts
+++ tests/integration/pages.test.ts
@@ -1,10 +1,12 @@
-// import request from 'supertest'
-// import app from '../../src/app'
+import request from 'supertest'
+import app from '../../src/app'
+import mongoose from 'mongoose'
 
 describe('Test the root path', () => {
   test('It should response the GET method', async () => {
-    // const response = await request(app).get('/')
-    // return expect(response.status).toEqual(200)
-    return expect(1 + 1).toEqual(2)
+    const response = await request(app).get('/v1/pages')
+    expect(response.status).toBe(200)
+    expect(1 + 1).toEqual(2)
+    await mongoose.connection.close()
   })
 })
