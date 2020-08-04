22a51f27e496805c6ebe212553b2bfb67ba9432a
nicknish09 <nnmarques97@gmail.com>
2020-08-04 17:50:15 -0300

    create action on todos controller emiting socket

--- src/app/controllers/TodosController.ts
+++ src/app/controllers/TodosController.ts
@@ -2,13 +2,16 @@ import { Request, Response } from 'express'
 
 import Todo from '../models/Todo'
 import { io } from '../../server'
-import { TODO_UPDATE_TYPE } from '../../config/constants'
+import { TODO_CREATE_TYPE, TODO_UPDATE_TYPE } from '../../config/constants'
 
 class TodosController {
   public async create (req: Request, res: Response): Promise<Response> {
     try {
       const todo = await Todo.create(req.body)
 
+      if (todo) {
+        io.emit(`${TODO_CREATE_TYPE}${todo.page}`, { todo })
+      }
       return res.status(200).send({ todo })
     } catch (err) {
       console.log(err)
